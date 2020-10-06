322c698f4716530eb5ae4e37e2225c77c4bc5a74
ferrandis24 <45199162+ferrandis24@users.noreply.github.com>
2020-10-06 23:55:48 +0200

    AE1-1

---
+++ Main.java
@@ -0,0 +1,13 @@
+package es.florida;
+
+
+
+	public class Main {
+		public static void main(String[] args) {
+			sayHello();
+		}
+
+		public static void sayHello(){
+			System.out.println("Hello World!");
+		}
+	}
